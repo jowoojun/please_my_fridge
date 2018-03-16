26e8eb074fa8437125040418c00e746a31a56d2a
“jowoojun” <jowoojun@naver.com>
2018-03-16 18:28:53 +0900

    added google cloud example

---
+++ visionAPI.py
@@ -0,0 +1,21 @@
+import os
+
+import google.cloud.storage
+
+# Create a storage client.
+storage_client = google.cloud.storage.Client()
+
+# TODO (Developer): Replace this with your Cloud Storage bucket name.
+bucket_name = 'my-bucket'
+bucket = storage_client.get_bucket(bucket_name)
+
+# TODO (Developer): Replace this with the name of the local file to upload.
+source_file_name = 'Local file to upload, for example ../document/1.txt'
+blob = bucket.blob(os.path.basename(source_file_name))
+
+# Upload the local file to Cloud Storage.
+blob.upload_from_filename(source_file_name)
+
+print('File {} uploaded to {}.'.format(source_file_name, bucket))
+
+
