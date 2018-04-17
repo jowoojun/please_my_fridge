92bfd134d6458d3244e530e1cc14d3ab1af58a6b
“jowoojun” <jowoojun@naver.com>
2018-04-17 21:21:27 +0900

    edited mongoDB envirment and translate.py

--- recipe/arrange_tables.py
+++ recipe/arrange_tables.py
@@ -25,29 +25,22 @@ recipes = db.full_recipes
 
 print("Starting Data Process")
 for i, recipe in enumerate(recipes.find()):
-    #j = i + 1
-    #if "title" in recipe:
-    #    print(j ,"st, title : " ,MyPrettyPrinter().pprint(recipe['title']))
-    #title = recipe["title"].replace('"', '').replace('\n','').replace(',','').encode("utf-8")
-    #recipe.update({"title": title}, upsert=False)
-    #recipes.save(recipe)
-
-    #title = recipe["title"]
-    #if "_id" in recipe:
-    #    title = '"' + title + '"'
-    #    print(title)
-    #    recipe.update({"title": title[1:]}, upsert=False)
-    #    #recipes.save(recipe)
+    """
+    title = recipe["title"].replace('"', '').replace('\n','').replace(',','').encode("utf-8")
+    recipe.update({"title": title}, upsert=False)
+    recipes.save(recipe)
+    """
+    """
     title = recipe["title"]
-    if " " == title[-1]:
-        print(title)
-        title = title[:-1]
+    if "`" in title:
+        title = title.replace("`", "'")
         recipe.update({"title": title}, upsert=False)
         recipes.save(recipe)
-    
-    #recipe.update({"url": ""})
-    #recipes.save(recipe)
-
+    """
+    """
+    recipe.update({"url": ""})
+    recipes.save(recipe)
+    """
 
 
 
@@ -56,51 +49,9 @@ for i, recipe in enumerate(recipes.find()):
     #ap = "googleimagesdownload -k " + '"' + recipe["title"].encode("utf-8") + '"' + " -l 1"
     #print(ap)
 
-
+new_dic = translate_text('ko', recipes.find_one())
+print(new_dic.encode("utf-8"))
 
 #print(count)
 
-def translate_text(target, text):
-    """Translates text into the target language.
-    Target must be an ISO 639-1 language code.
-    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
-    """
-    translate_client = translate.Client()
-    del text['_id']
-    del text['calories']
-    del text['date']
-    del text['desc']
-    del text['fat']
-    del text['protein']
-    del text['rating']
-    del text['sodium']
-    
 
-    text = text
-    #text = text.decode('utf-8')
-        
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
-    new_dict = {}
-    for key, value in text.items():
-        if type(value) == list:
-            text = []
-            for i in range(len(value)):
-                result = translate_client.translate(value[i], target_language=target)
-                #print(u'Text: {}'.format(result['input']))
-                #print(u'Translation: {}'.format(result['translatedText']))
-                #print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
-                text.append(result['translatedText'])
-            new_dict[key] = text
-                
-        else:
-            result = translate_client.translate(value, target_language=target)
-            #print(u'Text: {}'.format(result['input']))
-            #print(u'Translation: {}'.format(result['translatedText']))
-            #print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
-            new_dict[key] = result['translatedText']
-
-    return new_dict
-
-#new_dic = translate_text('ko', recipe.find_one())
-#MyPrettyPrinter().pprint(new_dic)
--- recipe/translate.py
+++ recipe/translate.py
@@ -5,6 +5,7 @@ import argparse
 from google.cloud import translate
 import six
 
+import os
 import pprint
 class MyPrettyPrinter(pprint.PrettyPrinter):
     def format(self, _object, context, maxlevels, level):
@@ -14,44 +15,57 @@ class MyPrettyPrinter(pprint.PrettyPrinter):
             _object = unicode(_object,'utf8')
             return "'%s'" % _object.encode('utf8'), True, False
         return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
-client = MongoClient('localhost', 27017)
 
-db = client['recipe_default_info']
-recipes = db.full_format_recipes
+print("connecting DB")
+client = MongoClient('ds119049.mlab.com', 19049)
 
-count = 0
-for i, recipe in enumerate(recipes.find()):
-    #if "title" in recipe:
-    #    print(j ,"st, title : " ,MyPrettyPrinter().pprint(recipe['title']))
+print("connecting collections")
+db = client['please_my_fridge']
+db.authenticate(os.environ["mongoID"], os.environ["mongoPW"])
+recipes = db.full_recipes
+
+def translate_text(target, text):
+    """Translates text into the target language.
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    translate_client = translate.Client()
+    del text['_id']
+    del text['calories']
+    del text['date']
+    del text['desc']
+    del text['fat']
+    del text['protein']
+    del text['rating']
+    del text['sodium']
+    del text['upsert']
     
-    title = recipe["title"].replace('"', '').replace('\n','').replace(',','').replace("`","'").encode("utf-8")
-    recipe.update({"title": title}, upsert=False)
-    recipes.save(recipe)
 
-    #title = recipe["title"]
-    #if "_id" in recipe:
-    #    title = '"' + title + '"'
-    #    print(title)
-    #    recipe.update({"title": title[1:]}, upsert=False)
-    #    #recipes.save(recipe)
-    #if " " in title[-1]:
-    #    title = '"' + title + '"'
-    #    print(title)
-    #    recipe.update({"title": title}, upsert=False)
-    #    recipes.save(recipe)
-    
-    #recipe.update({"url": ""})
-    #recipes.save(recipe)
+    text = text
+    #text = text.decode('utf-8')
+        
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    new_dict = {}
+    for key, value in text.iteritems():
+        if type(value) == list:
+            text = []
+            for i, val in enumerate(value):
+                result = translate_client.translate(val, target_language=target)
+                #print(u'Text: {}'.format(result['input']))
+                #print(u'Translation: {}'.format(result['translatedText']))
+                #print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
+                text.append(result['translatedText'])
+            new_dict[key] = text
+                
+        else:
+            result = translate_client.translate(value, target_language=target)
+            #print(u'Text: {}'.format(result['input']))
+            #print(u'Translation: {}'.format(result['translatedText']))
+            #print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
+            new_dict[key] = result['translatedText']
 
+    return new_dict
 
-
-
-
-    #print "googleimagesdownload -k \"", recipe["title"].encode("utf-8"),"\" -l 1"
-    #ap = "googleimagesdownload -k " + '"' + recipe["title"].encode("utf-8") + '"' + " -l 1"
-    #print(ap)
-
-
-
-#print(count)
-
+new_dic = translate_text('ko', recipes.find_one())
+MyPrettyPrinter().pprint(new_dic)
